f4e7a9d37192623c26cc7939f20889832622563b
coonik <vuonglongxungba@gmail.com>
2020-12-13 14:06:13 +0700

    done products page

--- src/app/components/products-page/products-page.component.ts
+++ src/app/components/products-page/products-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import DATA from 'src/assets/const/data.const';
 import IData from 'src/assets/interface/data.interface';
+import IProduct from 'src/assets/interface/product.interface';
 import IProductsParams from 'src/assets/interface/products-params.interface';
 
 @Component({
@@ -11,6 +12,8 @@ import IProductsParams from 'src/assets/interface/products-params.interface';
 })
 class ProductsPageComponent implements OnInit {
   private data: IData;
+
+  public searchName: string;
 
   public paginatedData: IData;
 
@@ -24,13 +27,31 @@ class ProductsPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params: IProductsParams) => {
-      this.data = DATA[`${params.pageName}`];
+      this.searchName = params.searchName;
+      if (this.searchName) {
+        let dataTemp: IProduct[] = [];
+        Object.keys(DATA).forEach((key) => {
+          dataTemp = [
+            ...dataTemp,
+            ...(DATA[`${key}`].products as IProduct[]).filter((product) =>
+              this.convertViToEn(product.name).includes(this.convertViToEn(this.searchName)),
+            ),
+          ];
+        });
+
+        this.data = {
+          name: 'Kết quả tìm kiếm:',
+          products: dataTemp,
+        };
+      } else {
+        this.data = DATA[`${params.pageName}`];
+      }
       this.totalItems = this.data.products.length;
       this.pageChanged();
     });
   }
 
-  pageChanged(event?) {
+  public pageChanged(event?): void {
     if (event) {
       this.currentPage = event.page;
     }
@@ -43,6 +64,23 @@ class ProductsPageComponent implements OnInit {
       ),
     };
   }
+
+  private convertViToEn = (str: string): string => {
+    return (
+      str
+        .toLowerCase()
+        .replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a')
+        .replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e')
+        .replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+        .replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o')
+        .replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u')
+        .replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+        .replace(/đ/g, 'd')
+        // Some system encode vietnamese combining accent as individual utf-8 characters
+        .replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, '') // Huyền sắc hỏi ngã nặng
+        .replace(/\u02C6|\u0306|\u031B/g, '') // Â, Ê, Ă, Ơ, Ư
+    );
+  };
 }
 
 export default ProductsPageComponent;
