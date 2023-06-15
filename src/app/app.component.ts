5969b7c2da405769c190d1ba8063491f8aa50199
tu nguyen <tunguyen@novahub.vn>
2023-06-15 09:52:11 +0700

    Set static title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export default class AppComponent {
-  private title = 'Đồ chơi công viên, trẻ em giá rẻ ';
-
-  constructor() {
-    this.changeTitle();
-  }
-
-  private changeTitle(): void {
-    setInterval(() => {
-      document.title = this.newTitle;
-      this.title = this.newTitle;
-    }, 10);
-    
-  }
-
-  private get newTitle(): string {
-    const firstCharacter = String.fromCharCode(this.title.charCodeAt(0));
-    return `${this.title.slice(1)}${firstCharacter}`;
-  }
 }
